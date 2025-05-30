5345222c39bf49a3adc3f136618493f5c3a57044
Arka1803 <104690100+Arka1803@users.noreply.github.com>
2025-05-30 18:40:04 +0000

    Deploying to gh-pages from @ Arka1803/arka1803.github.io@ccd2f591e2ec4a1b14fffedd67b4560d298d6860 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,9 +44,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+        },{id: "nav-teaching-assistantship-roles",
+          title: "Teaching Assistantship Roles",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
